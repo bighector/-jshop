804bc529f75f00a3841c4b2f161e1402c87dbb2f
fa20091001 <fa20091001@163.com>
2016-01-09 02:43:21 +0800

    解决友链报错,并且测试Git操作!

--- src/main/java/net/jeeshop/web/controller/manage/friendslinks/FriendsLinkController.java
+++ src/main/java/net/jeeshop/web/controller/manage/friendslinks/FriendsLinkController.java
@@ -1,7 +1,14 @@
 package net.jeeshop.web.controller.manage.friendslinks;
+
+import com.google.common.collect.Lists;
+import net.jeeshop.biz.base.bean.PageBean;
+import net.jeeshop.biz.base.bean.PageQueryBean;
 import net.jeeshop.biz.base.service.BaseService;
+import net.jeeshop.biz.cms.bean.ArticleCatalogBean;
 import net.jeeshop.biz.cms.model.Friendlink;
 import net.jeeshop.biz.cms.model.FriendlinkExample;
+import net.jeeshop.biz.cms.model.Hotquery;
+import net.jeeshop.biz.cms.model.HotqueryExample;
 import net.jeeshop.biz.cms.service.FriendsLinkService;
 import net.jeeshop.web.controller.manage.ManageBaseController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +18,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 /**
  * Created by machenike on 2016/1/6.
  */
 @Controller
-@RequestMapping("/manage/friendslink")
+@RequestMapping("/manage/navigation")
 public class FriendsLinkController extends ManageBaseController<Friendlink, FriendlinkExample> {
 
     @Autowired
@@ -27,7 +36,16 @@ public class FriendsLinkController extends ManageBaseController<Friendlink, Frie
         super.page_toList = page_toList;
         super.page_toEdit = page_toEdit;
     }
+    @Override
+    public BaseService<Friendlink, FriendlinkExample> getService() {
+        return friendsLinkService;
+    }
+    @Override
+    @RequestMapping("selectList")
+    public String selectList(ModelMap modelMap) {
+        return page_toList;
+    }
+
 }
 
 
-}
